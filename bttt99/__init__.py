c1107d9f8d931144a5067e38792caa7a72fde740
babykick <babykick@163.com>
2017-11-02 08:49:18 +0800

    update

--- bttt99/__init__.py
+++ bttt99/__init__.py
@@ -2,3 +2,13 @@ __author__ = """Stephen Chen"""
 __email__ = 'noemail@noemail.com'
 __version__ = '0.1'
 
+from .extractors.gaoqingfm import Gaoqingfm
+
+
+EX_mapping = {
+    'gaoqingfm': Gaoqingfm
+}
+
+
+def get_extractor(name):
+    return EX_mapping.get(name)
--- bttt99/core.py
+++ bttt99/core.py
@@ -1,9 +1,14 @@
 """
   API and command-line interface
 """
+from . import get_extractor
+
+default_extractor = 'gaoqingfm'
 
 
 def search(keyword):
-    from .extractors.gaoqingfm import Gaoqingfm
-    extractor = Gaoqingfm()
+    Extractor = get_extractor(default_extractor)
+    if not Extractor:
+        raise TypeError('No proper extractor found by {}'.format(default_extractor))
+    extractor = Extractor()
     extractor.fetch(keyword)
